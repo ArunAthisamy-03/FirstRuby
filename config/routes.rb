d672aa3e2fda8e02c390ccb42e0b1ee6a100216e
Arun Athisamy <unitearun@gmail.com>
2017-05-31 19:37:53 -0300

    added new landing page

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Set the root of the application
+	root 'static_pages#home'
+end
